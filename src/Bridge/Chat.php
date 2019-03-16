16504e7950e3bd1203627d2477dd992f7ff218c5
asatretdinov <asatretdinov@plesk.com>
2019-03-16 19:18:05 +0700

    remove unused "bot" logic

--- src/Bridge/Chat.php
+++ src/Bridge/Chat.php
@@ -3,10 +3,7 @@
 namespace Inquirer\Bridge;
 
 use Inquirer\Api;
-use Inquirer\Entity;
-use Inquirer\Factory;
 use Inquirer\Registry;
-use Inquirer\Exception;
 use Inquirer\Entity\ConversationItem;
 
 /**
@@ -56,16 +53,6 @@ class Chat
     public function confirmAnswer($callbackId)
     {
         $this->api->answerCallbackQuery($callbackId);
-    }
-
-    /**
-     * @return Entity\Bot
-     * @throws Exception\StorageException
-     */
-    protected function getBot()
-    {
-        $botFactory = new Factory\Bot(Registry::getInstance()->getApp()['botStorage']);
-        return $botFactory->getByUsername($this->chat->getBotUsername());
     }
 
     protected function getDialogStoragePath($dialogName)
